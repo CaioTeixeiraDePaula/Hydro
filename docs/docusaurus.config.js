7b0d961f15e9835fa88e42731bc5fee77cc2c6ac
CaioTeixeiraDePaula <caio.paula@sou.inteli.edu.br>
2024-03-21 13:08:40 -0300

    banana

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -9,21 +9,21 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Hydro Corp.',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Learning the Future',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://FHydro.example.com',
+  url: 'https://Hydro.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Hydro',
+  baseUrl: '/projects/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Hydro', // Usually your GitHub org/user name.
-  projectName: 'Hydro family', // Usually your repo name.
+  projectName: 'Hydro', // Usually your repo name.
 
-  onBrokenLinks: 'warn',
+  onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
@@ -47,21 +47,13 @@ const config = {
             'https://github.com/CaioTeixeiraDePaula',
             routeBasePath: '/'
         },
-        blog: {
-          showReadingTime: false,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-            routeBasePath: '/',
-        },
+        blog: false,
         theme: {
           customCss: './src/css/custom.css',
         },
       }),
     ],
   ],
-
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
@@ -69,7 +61,7 @@ const config = {
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Hydro Logo',
           src: 'img/Hydro Logo.svg',
         },
         items: [
@@ -87,47 +79,26 @@ const config = {
         ],
       },
       footer: {
-        style: 'dark',
+        style: 'light',
         links: [
           {
-            title: 'Docs',
+            title: 'Developers',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Caio Teixeira de Paula',
+                to: "/"
               },
             ],
           },
           {
-            title: 'Community',
+            title: 'Contacts',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
+                label: "LinkdIn",
+                href: "https://www.linkedin.com/in/caio-teixeira-paula/"
+              }
+            ]
+          }
         ],
         copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
